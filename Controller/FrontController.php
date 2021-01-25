34be9d7d485cf4cd8bc70e015874cd8d55a6c330
Pitel Francois <francoispitel87@gmail.com>
2021-01-25 16:33:37 +0100

    ajout de la session dans View et FrontController

--- Controller/FrontController.php
+++ Controller/FrontController.php
@@ -8,11 +8,13 @@ namespace Francois\Projet\Controller ;
 
 require_once 'Model/Post.php' ;
 require_once 'Framework/View.php' ;
+require_once "Framework/Session.php" ;
 
 // USE
 
 use Francois\Projet\Model\Post ;
 use Francois\Projet\Framework\View ;
+use \Francois\Projet\Framework\Session ;
 
 // DECLARATION DE LA CLASSE
 
@@ -20,12 +22,14 @@ class FrontController {
 
 	private $post ;
 	private $view ;
+	private $session ;
 
 	// LE CONSTRUTEUR
 
 	public function __construct() {
         $this->post = new Post() ;
         $this->view = new View() ;
+        $this->session = new Session($this->session) ;
     }
 
     // METHODE QUI RENVOIE SUR LA PAGE D'ACCUEIL AVEC LE DERNIER CHAPITRE
--- Framework/View.php
+++ Framework/View.php
@@ -2,17 +2,27 @@
 
 namespace Francois\Projet\Framework ;
 
+require_once "Framework/Session.php" ;
+
+use \Francois\Projet\Framework\Session ;
+
 class View {
 
 	private $file ;
 	private $title ;
+	private $session ;
+
+	public function __construct() {
+		$this->session = new Session($this->session) ;
+	}
 
 	public function renderFront($template, $data = []) {
 		$this->file = 'View/Frontend/view' . $template . '.php' ;
 		$content = $this->renderFile($this->file, $data) ;
 		$view = $this->renderFile('View/Frontend/viewFrontTemplate.php', [
 				'title' => $this->title,
-				'content' => $content 
+				'content' => $content,
+				'session' => $this->session
 		]) ;
 	echo $view ;
 	}
@@ -22,7 +32,8 @@ class View {
 		$content = $this->renderFile($this->file, $data) ;
 		$view = $this->renderFile('View/Backend/viewBackTemplate.php', [
 				'title' => $this->title,
-				'content' => $content 
+				'content' => $content,
+				'session' => $this->session  
 		]) ;
 	echo $view ;
 	}
